f8c805b70ef3d5d029cdc6126d7a0066a277fc3b
Nidlow155 <mtt.lindow@gmail.com>
2021-03-30 09:36:51 -0600

    Framework for request methods implemented.

--- http.cpp
+++ http.cpp
@@ -6,10 +6,17 @@
 #include <iterator>
 #include <string>
 #include <ctime>
+#include <vector>
+#include <chrono>
 
 #define MCW MPI_COMM_WORLD
 
 using namespace std;
+
+unsigned long getTime()
+{
+  return std::chrono::system_clock::now().time_since_epoch() / std::chrono::milliseconds(1);
+}
 
 map<string, int> initializeServerData()
 {
@@ -34,14 +41,69 @@ string generateRandomLetter()
   return s;
 }
 
+// TODO: implement method
+void request_102()
+{
+
+}
+
+// TODO: implement method
+void request_200()
+{
+
+}
+
+// TODO: implement method
+void request_404()
+{
+
+}
+
+// TODO: implement method
+void request_406()
+{
+
+}
+
+// TODO: implement method
+void request_408()
+{
+
+}
+
+// TODO: implement method
+void request_429()
+{
+
+}
+
+// TODO: impelment method
+void request_507()
+{
+
+}
+
 int main(int argc, char **argv)
 {
   srand(time(0));
   map<string, int> initializeServerData();
   string generateRandomLetter();
+  unsigned long getTime();
+  void request_102();
+  void request_200();
+  void request_404();
+  void request_406();
+  void request_408();
+  void request_429();
+  void request_507();
 
-  int rank, size;
+  int rank, size, request;
+  unsigned long startTime, endTime;
   map<string, int> serverData;
+  // Each process records how long it takes for a request to be completed : will be averaged later
+  vector<int> averageRequestTime;
+  // Process 0 records average time to process request : we could do this per request type : will be averaged later
+  vector<int> averageProcessingTime;
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MCW, &rank);
   MPI_Comm_size(MCW, &size);
@@ -60,8 +122,57 @@ int main(int argc, char **argv)
   // All other processes request from the server
   else
   {
+    // TODO: implement methods above
+    request = rand() % 7;
+    switch(request)
+    {
+      case 0:
+        startTime = getTime();
+        request_102();
+        endTime = getTime();
+        averageRequestTime.push_back(endTime - startTime);
+        break;
+      case 1:
+        startTime = getTime();
+        request_200();
+        endTime = getTime();
+        averageRequestTime(endTime - startTime);
+        break;
+      case 2:
+        startTime = getTime();
+        request_404();
+        endTime = getTime();
+        averageRequestTime(endTime - startTime);
+        break;
+      case 3:
+        startTime = getTime();
+        request_406();
+        endTime = getTime();
+        averageRequestTime(endTime - startTime);
+        break;
+      case 4:
+        startTime = getTime();
+        request_408();
+        endTime = getTime();
+        averageRequestTime(endTime - startTime);
+        break;
+      case 5:
+        startTime = getTime();
+        request_429();
+        endTime = getTime();
+        averageRequestTime(endTime - startTime);
+        break;
+      case 6:
+        startTime = getTime();
+        request_507();
+        endTime = getTime();
+        averageRequestTime(endTime - startTime);
+        break;
+    }
 
   }
+
+  // TODO: Average time values and display that along with other interesting data
 
   MPI_Finalize();
 
